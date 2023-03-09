63e6af1d0df17153fb2a0361363e9cd046a1ec64
Parvesh Saini <97528080+parveshsaini@users.noreply.github.com>
2023-03-09 20:56:10 +0530

    Factorials of large numbers [GFG]

---
+++ ARRAYS/factorialOfLargeNumber.cpp
@@ -0,0 +1,32 @@
+// Given an integer N, find its factorial. 
+// return a list of integers denoting the digits that make up the factorial of N.
+
+class Solution {
+public:
+    vector<int> factorial(int N){
+        vector<int> ans;
+        ans.push_back(1);
+        int carry=0;
+        
+        //STORING ANS DIGIT WISE
+        for(int i=2; i<=N; i++){
+            for(int j=0; j<ans.size(); j++){
+                int x= ans[j]*i + carry;
+                ans[j]= x%10;
+                carry= x/10;
+            }   
+            
+            //STORING CARRY DIGIT WISE
+            while(carry){
+                ans.push_back(carry%10);
+                carry= carry/10;
+            }
+                
+            
+        }
+        
+        //REVERSING THE ARRAY TO GET FINAL ANSWER
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
