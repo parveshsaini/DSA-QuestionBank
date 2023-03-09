217d4a0e9d61f1f72d86fafc8c2645930e54262a
Parvesh Saini <97528080+parveshsaini@users.noreply.github.com>
2023-03-09 14:27:08 +0530

    Wave Print

---
+++ ARRAYS/wavePrintMatrix.cpp
@@ -0,0 +1,26 @@
+// Given an m x n matrix, return all elements of the matrix in wave order.
+
+vector<int> ans;
+    int i=0, j=0;
+
+    while(j<mCols){
+
+        //IF COL NUMBER IS EVEN, ROW INCREASES FROM 0
+        if(j%2==0){
+            for(i=0; i<nRows; i++){               
+                ans.push_back(arr[i][j]);          
+            }
+            j++;
+        }
+
+        //IF COL NUMBER IS ODD, ROW DECREASES FROM LAST INDEX
+        else{
+            for(i=nRows-1; i>=0; i--){              
+                ans.push_back(arr[i][j]);           
+            }
+            j++;
+        }
+
+    }
+    
+    return ans;
